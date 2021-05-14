33648dc8e8353c160163b88756ef6bada0eceef3
diwakar <diwakarsingh052@gmail.com>
2021-05-14 15:07:20 +0530

    .

--- 4-concurrency/urls.go
+++ 4-concurrency/urls.go
@@ -19,18 +19,19 @@ type HttpResponse struct {
 }
 
 func httpGet(urls []string) []*HttpResponse {
-	ch := make(chan *HttpResponse)
+	ch := make(chan *HttpResponse) // create a channel to store responses
 
 	responses := []*HttpResponse{}
 
-	client := http.Client{}
+	client := http.Client{} // creates an http client
 	for _, url := range urls {
 
 		go func(url string) { // Number of goroutines 3
 
 			fmt.Println("fetching Url", url)
-			resp, err := client.Get(url)
-			ch <- &HttpResponse{
+			resp, err := client.Get(url) // do a get request
+
+			ch <- &HttpResponse{ // sending data in channel
 				url:      url,
 				response: resp,
 				err:      err,
@@ -40,7 +41,7 @@ func httpGet(urls []string) []*HttpResponse {
 	}
 
 	for {
-
+		// waiting for go routines to put data in channel
 		select {
 		case r := <-ch:
 			fmt.Printf("%s was fetched\n", r.url)
@@ -53,6 +54,7 @@ func httpGet(urls []string) []*HttpResponse {
 				return responses
 			}
 
+		// after 10 millisecond do something
 		case <-time.After(10 * time.Millisecond):
 			fmt.Printf(".")
 
@@ -65,8 +67,8 @@ func httpGet(urls []string) []*HttpResponse {
 func result(res []*HttpResponse) {
 	fmt.Println("Output from result func")
 	for _, u := range res {
-		if u.response != nil {
-			fmt.Println(u.response.Status)
+		if u.response != nil { // if a domain is not found then response is nil
+			fmt.Println(u.url, u.response.Status)
 		} else {
 			fmt.Println(u.url, "not found")
 
